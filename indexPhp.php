601d11c49ea24aed28e306b999eb097dcfe5fca1
cosmonet-fr <60877684+cosmonet-fr@users.noreply.github.com>
2020-02-25 16:44:24 +0100

    init

---
+++ indexPhp.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Curriculum vitæ</title>
+    <link rel="stylesheet" href="css/<?php print $_GET['stylesheet'] ?>/master.css">
+  </head>
+  <body>
+    <div id="body">
+		<div class="left">
+		<aside class="">
+			<img class="pic" src="img/telemaque.jpg" alt="Photo de Profile">
+			<h3>Contact</h3>
+			<p>Adresse: <br> Ithaque, Grèce </p>
+			<p>Téléphone: <br> 06 00 00 00 00 </p>
+			<p>e-mail: <br> telemaque@ithaque.gr </p>
+			<p>Site Web: <br> www.ithaque.gr </p>
+
+			<h3>A propos de moi</h3>
+			<p>Télémaque est le fils d'Ulysse, roi de la petite île grecque d'Ithaque, et de Pénélope. Télémaque est encore un tout jeune enfant quand son père Ulysse part pour la guerre de Troie.</p>
+
+		</aside>
+
+		</div>
+		<div class="right">
+		<header>
+			<h1><span>Télémaque</span> Odysseus</h1>
+			<h2><span>Dévelo</span>ppeur web en freelance</h2>
+		</header>
+
+			<article class="formation">
+			<h3>Formation</h3>
+			<hr>
+
+			<div class="event">
+				<div class="date first">
+				<ul>
+					<li>2016 - 2017 <br> Paris </li>
+				</ul>
+				</div>
+				<div class="abstract">
+				<h2>OpenClassrooms</h2>
+				<h3>Développeur Web Junior</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit. </p>
+				</div>
+			</div>
+
+			<div class="event">
+				<div class="date first">
+				<ul>
+					<li>2014 - 2016 <br> Ithaque </li>
+				</ul>
+				</div>
+				<div class="abstract">
+				<h2>Université d'Apollon</h2>
+				<h3>Designer Graphique </h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit. </p>
+				</div>
+			</div>
+
+			</article>
+			<article class="expPro">
+			<h3>Expérience Professionelle</h3>
+			<hr>
+			<div class="event">
+				<div class="date first">
+				<ul>
+					<li>Mars 2018 - Poste actuel <br> Ithaque </li>
+				</ul>
+				</div>
+				<div class="abstract">
+				<h2>Agence Web d'Ithaque</h2>
+				<h3>Développeur Web Full Stack</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit. </p>
+				</div>
+			</div>
+
+			<div class="event">
+				<div class="date first">
+				<ul>
+					<li>Avril 2017 - Mars 2018 <br> Ithaque </li>
+				</ul>
+				</div>
+				<div class="abstract">
+				<h2>Agence Web Bidule</h2>
+				<h3>Développeur Web</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit. </p>
+				</div>
+			</div>
+
+			</article>
+
+		</div>
+    </div>
+    <div id="footer">
+      <div class="left">
+        <h3>Loisirs</h3>
+        <div class="hobbies">
+          <img src="img/icon/bow.svg" alt="Tire à l'arc">
+          <img src="img/icon/swimming.svg" alt="Natation">
+        </div>
+      </div>
+      <div class="right">
+        <article class="recommandation">
+  			<h3>Recommandation</h3>
+  			<hr>
+  			<div class="boxRecommandation">
+
+  				<div class="eventRecommandation">
+  				<h3>MBECHEZI Mlanawo</h3>
+  				<p>Mentor Chez OpenClassrooms</p>
+  				<p>Téléphon:</p>
+  				<p>e-mail:</p>
+  				</div>
+
+  				<div class="eventRecommandation">
+  				<h3>Mentor</h3>
+  				<p>Présepteur</p>
+  				<p>Téléphon:</p>
+  				<p>e-mail:</p>
+  				</div>
+  			</div>
+
+  			</article>
+
+      </div>
+
+    </div>
+    <!--<footer><p>Fait par Stéphane BILLOIS dans le cadre d'une formation OpenClassrooms. Code sous licence GNU Afero GPL. Les images des loisirs viennent du site freesvg.org sous licence CC0</p></footer>-->
+  </body>
+</html>
